731d1f7785de8c6541330eb139d2d2bdaae859bf
jliu212 <jiangzhouliu1@gmail.com>
2025-06-13 16:01:57 -0400

    Added nx, omnisharp plugin configs

--- dot_config/nvim/lua/plugins/colortheme.lua
+++ dot_config/nvim/lua/plugins/colortheme.lua
@@ -10,11 +10,16 @@ return {
   --   },
   -- },
   --
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+  },
   {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "catppuccin-frappe",
+      --   colorscheme = "catppuccin-mocha",
     },
   },
 }
